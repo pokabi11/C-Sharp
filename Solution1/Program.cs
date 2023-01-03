6b82ae44c519cd70cc2d2eb64df30eac09b96e2d
pokabi <hlong2k1@gmail.com>
2023-01-03 11:32:44 +0700

    test

---
+++ Solution1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Solution1.session1;
+
+public class Program
+
+{
+public static void Main(string[] args)
+    {
+        int x = 10;
+        double y = 3.14;
+        Console.WriteLine("Hello xd " + x);
+        int[] ns = { 1, 3, 5, 6, 7 };
+        foreach (int i in ns) { Console.WriteLine(i); }
+        for(int i = 0; i < ns.Length; i++) { Console.WriteLine(i); }
+        
+        string input = Console.ReadLine(); //scan
+        int num = Convert.ToInt32(input);
+        Console.WriteLine("num = " + num );
+
+        Human h = new Human();
+        h.name = "aaaaaaaaaaaaaaaaaaaaaaa";
+        
+    }
+
+}
---
+++ Solution1/session1/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Solution1.session1
+{
+	public class Human
+	{
+		public Human(){ }
+		public string name;
+		public int age;
+		public int height;
+		public int weight;
+
+		public void Eat()
+		{
+			Console.WriteLine("Eating " + name);
+		}
+
+		public void Drink()
+		{
+			Console.WriteLine(this.name + " is a " + age + " years old");
+		}
+	}
+}
+
